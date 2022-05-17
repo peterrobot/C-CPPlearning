0742d35bc9a21d21f030ba6541396341eb1f2978
peterrobot <ezio_da_firenze@outlook.com>
2022-05-17 21:15:47 +0800

    standardlized

--- lib/Test_environment/main.cpp
+++ lib/Test_environment/main.cpp
@@ -4,40 +4,45 @@
 
 using namespace std;
 
-class Point
+class point
 {
 private:
     int _x;
     int _y;
 public:
-    explicit Point(int x, int y) : _x(x), _y(y)
+    explicit point(const int x,const int y) : _x(x), _y(y)
     {
         #ifdef DEBUG
         std::cout << "Initializer is called\n";
         #endif
     }
-    Point(const Point& P)
+    point(const point& p)
     {
         #ifdef DEBUG
         std::cout << "Copy constructor is called\n";
         #endif
-        _x = P._x * 2;
-        _y = P._y * 2;
+        _x = p._x * 2;
+        _y = p._y * 2;
     }
-    void Print()
+    point(point&&) = default;
+    ~point() = default;
+	point& operator=(const point&) = default;
+    point& operator=(point&&) = default;
+
+    void Print() const
     {
         std::cout << _x << "," << _y << std::endl;
     }
 };
 
-void func(Point p)
+void func(point p)
 {
     p.Print();
 }
 
 constexpr struct funcObj{
-    template <typename _Arg,typename _Ret = _Arg >
-    _Ret operator()(_Arg &arg1, _Arg &arg2) const {
+    template <typename Arg,typename Ret = Arg >
+    Ret operator()(Arg &arg1, Arg &arg2) const {
         return arg1+arg2;
     }
 }funcAdd{};
@@ -54,7 +59,7 @@ struct personal_cast_fn{
 template <typename T>
 constexpr personal_cast_fn<T> personal_cast{};
 
-int main()
+int main() noexcept
 {
     int a = 1, b = 2;
     short s = 10;
